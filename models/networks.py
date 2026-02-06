8af727287cd67fbb52d61f9aedbfb99d02c7740a
Janmesh Ukey <j.y.ukey@uva.nl>
2026-02-06 18:09:24 +0100

    FiLM based modulation - basic

--- models/networks.py
+++ models/networks.py
@@ -69,6 +69,23 @@ class Upsample2(nn.Module):
     def forward(self, x):
         return torch.nn.functional.interpolate(x, scale_factor=self.factor, mode=self.mode)
 
+class N_UpsampleBlock(nn.Module):
+    def __init__(self, scale_factor, input_dim, output_dim, activ, norm, num_classes):
+        super().__init__()
+        self.upsample = Upsample2(scale_factor=scale_factor)
+        self.conv = N_Conv2dBlock(
+            input_dim, output_dim,
+            5, 1, 2,
+            norm=norm,
+            activation=activ,
+            pad_type='reflect',
+            num_classes=num_classes
+        )
+
+    def forward(self, x, seg=None):
+        x = self.upsample(x)
+        x = self.conv(x, seg)
+        return x
 
 class Upsample(nn.Module):
     def __init__(self, channels, pad_type='repl', filt_size=4, stride=2):
@@ -173,6 +190,8 @@ def init_weights(net, init_type='normal', init_gain=0.02, debug=False):
     """
     def init_func(m):  # define the initialization function
         classname = m.__class__.__name__
+        if isinstance(m, FiLM):
+            return
         if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
             if debug:
                 print(classname)
@@ -191,6 +210,7 @@ def init_weights(net, init_type='normal', init_gain=0.02, debug=False):
         elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
             init.normal_(m.weight.data, 1.0, init_gain)
             init.constant_(m.bias.data, 0.0)
+            
 
     net.apply(init_func)  # apply the initialization function <init_func>
 
@@ -266,6 +286,9 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
     elif netG == 'resnet_seg':
         n_blocks = 9
         net = S_Resnet(input_nc, output_nc, num_downs=2, n_res=n_blocks - 4, ngf=ngf, norm='inst', nl_layer='relu', num_classes=opt.num_classes)
+    elif netG == 'resnet_seg_film':
+        n_blocks = 9
+        net = N_Resnet(input_nc, output_nc, num_downs=2, n_res=n_blocks - 4, ngf=ngf, norm='film', nl_layer='relu', num_classes=opt.num_classes)
     else:
         raise NotImplementedError('Generator model name [%s] is not recognized' % netG)
     return init_net(net, init_type, init_gain, gpu_ids, initialize_weights=('stylegan2' not in netG))
@@ -784,20 +807,46 @@ class S_Resnet(nn.Module):
             else:
                 return images_recon
 
+
+class N_Resnet(nn.Module):
+    def __init__(self, input_nc, output_nc, num_downs, n_res, num_classes, ngf=64,
+                 norm=None, nl_layer=None):
+        super(N_Resnet, self).__init__()
+        n_downsample = num_downs
+        pad_type = 'reflect'
+        self.enc_content = ContentEncoder(n_downsample, n_res, input_nc, ngf, 'inst', nl_layer, pad_type=pad_type)
+        self.dec = N_Decoder(n_downsample, n_res, self.enc_content.output_dim, output_nc, norm=norm, activ=nl_layer, pad_type=pad_type, nz=0, num_classes=num_classes)
+
+
+    def decode(self, content, seg_onehot):
+        d = self.dec(content, seg_onehot)
+        return d
+
+    def forward(self, image, seg, nce_layers=[], encode_only=False):
+        content, feats = self.enc_content(image, nce_layers=nce_layers, encode_only=encode_only)
+        if encode_only:
+            return feats
+        else:
+            images_recon = self.decode(content, seg)
+            if len(nce_layers) > 0:
+                return images_recon, feats
+            else:
+                return images_recon
+
 ##################################################################################
 # Encoder and Decoders
 ##################################################################################
 
 # --- Fixed S_Decoder: expose logical blocks (ModuleList) ---
 class S_Decoder(nn.Module):
-    def __init__(self, n_upsample, n_res, dim, output_dim, norm='batch', activ='relu', pad_type='zero', nz=0):
+    def __init__(self, n_upsample, n_res, dim, output_dim, norm='batch', activ='relu', pad_type='zero', nz=0, num_classes=None):
         super(S_Decoder, self).__init__()
 
         blocks = []
         self.in_channels_list = []
 
         # 1) ResBlocks as first logical block
-        blocks.append(ResBlocks(n_res, dim, norm, activ, pad_type=pad_type, nz=nz))
+        blocks.append(ResBlocks(n_res, dim, norm, activ, pad_type=pad_type, nz=nz, num_classes=num_classes))
         self.in_channels_list.append(dim)
 
         # 2) For each upsample: make a single Sequential block (Upsample + Conv block)
@@ -810,13 +859,13 @@ class S_Decoder(nn.Module):
             self.in_channels_list.append(input_dim)
             up_block = nn.Sequential(
                 Upsample2(scale_factor=2),
-                Conv2dBlock(input_dim, dim // 2, 5, 1, 2, norm='ln', activation=activ, pad_type='reflect')
+                Conv2dBlock(input_dim, dim // 2, 5, 1, 2, norm='ln', activation=activ, pad_type='reflect', num_classes=num_classes)
             )
             blocks.append(up_block)
             dim = dim // 2
 
         # 3) Final conv as last logical block
-        final_block = Conv2dBlock(dim, output_dim, 7, 1, 3, norm='none', activation='tanh', pad_type='reflect')
+        final_block = Conv2dBlock(dim, output_dim, 7, 1, 3, norm='none', activation='tanh', pad_type='reflect', num_classes=num_classes)
         blocks.append(final_block)
         self.in_channels_list.append(dim)
 
@@ -827,6 +876,61 @@ class S_Decoder(nn.Module):
         for block in self.blocks:
             x = block(x)
         return x
+
+
+class N_Decoder(nn.Module):
+    def __init__(self, n_upsample, n_res, dim, output_dim, norm='batch', activ='relu', pad_type='zero', nz=0, num_classes=None):
+        super(N_Decoder, self).__init__()
+
+        blocks = []
+        self.in_channels_list = []
+
+        # 1) ResBlocks as first logical block
+        blocks.append(N_ResBlocks(n_res, dim, norm, activ, pad_type=pad_type, nz=nz, num_classes=num_classes))
+        self.in_channels_list.append(dim)
+
+        # 2) For each upsample: make a single Sequential block (Upsample + Conv block)
+        for i in range(n_upsample):
+            if i == 0:
+                input_dim = dim + nz
+            else:
+                input_dim = dim
+            
+            self.in_channels_list.append(input_dim)
+            up_block = N_UpsampleBlock(2, input_dim, dim // 2, activ, norm, num_classes)
+            blocks.append(up_block)
+            dim = dim // 2
+
+        # 3) Final conv as last logical block
+        final_block = N_Conv2dBlock(dim, output_dim, 7, 1, 3, norm='none', activation='tanh', pad_type='reflect', num_classes=num_classes)
+        blocks.append(final_block)
+        self.in_channels_list.append(dim)
+
+        # store as ModuleList so blocks align 1:1 with in_channels_list
+        self.blocks = nn.ModuleList(blocks)
+
+    def forward(self, x, seg_onehot=None):
+        for block in self.blocks:
+            x = block(x, seg_onehot)
+        return x
+
+### FiLM
+class FiLM(nn.Module):
+    def __init__(self, num_classes, num_features):
+        super().__init__()
+        self.mlp = nn.Conv2d(num_classes, num_features * 2, kernel_size=3, padding=1)
+        
+ 
+        ### need to move this initialization
+        nn.init.zeros_(self.mlp.weight)
+        nn.init.zeros_(self.mlp.bias)
+
+    def forward(self, x, seg):
+        seg = F.interpolate(seg, size=x.shape[2:], mode='nearest')
+        gamma_beta = self.mlp(seg)
+        gamma, beta = gamma_beta.chunk(2, dim=1)
+
+        return gamma * x + beta
 
 
 
@@ -965,7 +1069,7 @@ class Decoder(nn.Module):
 
 
 class ResBlocks(nn.Module):
-    def __init__(self, num_blocks, dim, norm='inst', activation='relu', pad_type='zero', nz=0):
+    def __init__(self, num_blocks, dim, norm='inst', activation='relu', pad_type='zero', nz=0, num_classes=None):
         super(ResBlocks, self).__init__()
         self.model = []
         for i in range(num_blocks):
@@ -975,6 +1079,26 @@ class ResBlocks(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+
+class N_ResBlock(nn.Module):
+    def __init__(self, dim, norm='inst', activation='relu', pad_type='zero', nz=0, num_classes=None):
+        super().__init__()
+
+        self.blocks = nn.ModuleList([
+            N_Conv2dBlock(dim + nz, dim, 3, 1, 1,
+                          norm=norm, activation=activation,
+                          pad_type=pad_type, num_classes=num_classes),
+            N_Conv2dBlock(dim, dim + nz, 3, 1, 1,
+                          norm=norm, activation='none',
+                          pad_type=pad_type, num_classes=num_classes),
+        ])
+
+    def forward(self, x, seg=None):
+        residual = x
+        out = x
+        for block in self.blocks:
+            out = block(out, seg)
+        return out + residual
 
 ##################################################################################
 # Basic Blocks
@@ -1001,6 +1125,21 @@ class ResBlock(nn.Module):
         out += residual
         return out
 
+class N_ResBlocks(nn.Module):
+    def __init__(self, num_blocks, dim, norm='inst', activation='relu',
+                 pad_type='zero', nz=0, num_classes=None):
+        super().__init__()
+        self.blocks = nn.ModuleList([
+            N_ResBlock(dim, norm=norm, activation=activation,
+                       pad_type=pad_type, nz=nz, num_classes=num_classes)
+            for _ in range(num_blocks)
+        ])
+
+    def forward(self, x, seg=None):
+        out = x
+        for block in self.blocks:
+            out = block(out, seg)
+        return out
 
 class Conv2dBlock(nn.Module):
     def __init__(self, input_dim, output_dim, kernel_size, stride,
@@ -1055,6 +1194,63 @@ class Conv2dBlock(nn.Module):
             x = self.activation(x)
         return x
 
+class N_Conv2dBlock(nn.Module):
+    def __init__(self, input_dim, output_dim, kernel_size, stride,
+                 padding=0, norm='none', activation='relu', pad_type='zero', num_classes=None):
+        super(N_Conv2dBlock, self).__init__()
+        self.use_bias = True
+        # initialize padding
+        if pad_type == 'reflect':
+            self.pad = nn.ReflectionPad2d(padding)
+        elif pad_type == 'zero':
+            self.pad = nn.ZeroPad2d(padding)
+        else:
+            assert 0, "Unsupported padding type: {}".format(pad_type)
+
+        # initialize normalization
+        norm_dim = output_dim
+        if norm == 'batch':
+            self.norm = nn.BatchNorm2d(norm_dim)
+        elif norm == 'inst':
+            self.norm = nn.InstanceNorm2d(norm_dim, track_running_stats=False)
+        elif norm == 'ln':
+            self.norm = LayerNorm(norm_dim)
+        elif norm == 'none':
+            self.norm = None
+        elif norm == 'film':
+            self.norm = FiLM(num_classes=num_classes, num_features=norm_dim)
+        else:
+            assert 0, "Unsupported normalization: {}".format(norm)
+
+        # initialize activation
+        if activation == 'relu':
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == 'lrelu':
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        elif activation == 'selu':
+            self.activation = nn.SELU(inplace=True)
+        elif activation == 'tanh':
+            self.activation = nn.Tanh()
+        elif activation == 'none':
+            self.activation = None
+        else:
+            assert 0, "Unsupported activation: {}".format(activation)
+
+        # initialize convolution
+        self.conv = nn.Conv2d(input_dim, output_dim, kernel_size, stride, bias=self.use_bias)
+
+    def forward(self, x, seg=None):
+        x = self.conv(self.pad(x))
+        if self.norm:
+            if isinstance(self.norm, FiLM):
+                x = self.norm(x, seg)
+            else:   
+                x = self.norm(x)
+        if self.activation:
+            x = self.activation(x)
+        return x
 
 class LinearBlock(nn.Module):
     def __init__(self, input_dim, output_dim, norm='none', activation='relu'):
