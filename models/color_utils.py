58f9ff01b115efcb15664cc372978012f5df7bbf
Janmesh Ukey <j.y.ukey@uva.nl>
2026-02-13 11:30:31 +0100

    train state for gen_seg_style_loss

---
+++ models/color_utils.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+import functools
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+
+
+def gram_matrix(features):
+    """
+    Compute Gram matrix from feature maps
+    features: [B, C, H, W]
+    returns: [B, C, C]
+    """
+    B, C, H, W = features.size()
+    features = features.view(B, C, H * W)  # Flatten spatial dims
+    
+    # Compute correlation: F * F^T
+    gram = torch.bmm(features, features.transpose(1, 2))
+    
+    # Normalize by number of elements
+    gram = gram / (C * H * W)
+    
+    return gram
+
+
+class StyleLoss(nn.Module):
+    def __init__(self, layers=['conv1_2', 'conv2_2', 'conv3_3', 'conv4_3']):
+        super().__init__()
+        
+        # Use pretrained VGG for feature extraction
+        vgg = torchvision.models.vgg19(pretrained=True).features
+        self.vgg = vgg.eval()
+        
+        # Freeze VGG weights
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+        
+        # Define which layers to use
+        self.layer_names = layers
+        self.layer_indices = {
+            'conv1_2': 3,
+            'conv2_2': 8,
+            'conv3_3': 17,
+            'conv4_3': 26,
+            'conv5_3': 35
+        }
+    
+    def extract_features(self, x):
+        """Extract features from specified VGG layers"""
+        features = {}
+        for name, module in self.vgg._modules.items():
+            x = module(x)
+            idx = int(name)
+            
+            for layer_name, layer_idx in self.layer_indices.items():
+                if idx == layer_idx and layer_name in self.layer_names:
+                    features[layer_name] = x
+        
+        return features
+    
+    def forward(self, generated, target):
+        """
+        generated: [B, 3, H, W] - your generated RGB images
+        target: [B, 3, H, W] - real RGB images (can be unpaired!)
+        """
+        # Normalize for VGG (ImageNet stats)
+        mean = torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).to(generated.device)
+        std = torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).to(generated.device)
+        
+        gen_norm = (generated - mean) / std
+        target_norm = (target - mean) / std
+        
+        # Extract features
+        gen_features = self.extract_features(gen_norm)
+        target_features = self.extract_features(target_norm)
+        
+        # Compute Gram matrices and compare
+        style_loss = 0.0
+        
+        for layer_name in self.layer_names:
+            gen_gram = gram_matrix(gen_features[layer_name])
+            target_gram = gram_matrix(target_features[layer_name])
+            
+            style_loss += F.mse_loss(gen_gram, target_gram)
+        
+        return style_loss / len(self.layer_names)
--- models/cut_model.py
+++ models/cut_model.py
@@ -7,6 +7,7 @@ import util.util as util
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from . import color_utils
 
 
 class CUTModel(BaseModel):
@@ -71,6 +72,8 @@ class CUTModel(BaseModel):
 
         #self.loss_names += ['red'] #['red', 'color']
         #self.visual_names += ['edge_gen', 'edge_gt']
+        if self.isTrain:
+            self.loss_names += ['style']
 
         ## set default loss weights
         for name in self.loss_names:
@@ -114,6 +117,7 @@ class CUTModel(BaseModel):
             # define loss functions
             self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)
             self.criterionRed = torch.nn.MSELoss().to(self.device)
+            self.criterionStyle = color_utils.StyleLoss().to(self.device)
             self.criterionNCE = []
 
             for nce_layer in self.nce_layers:
@@ -262,9 +266,10 @@ class CUTModel(BaseModel):
 
         #self.loss_color = (self.criterionColor(self.fake_B, self.real_A_mask) + self.criterionColor(self.idt_B, self.real_B_mask) if self.opt.nce_idt else 0.0) * 1.0
         #self.loss_edge, self.edge_gen, self.edge_gt = self.criterionEdge(self.real_A, self.real_A_mask, self.fake_B)
-                                                
 
-        self.loss_G = self.loss_G_GAN + loss_NCE_both #+ self.loss_red #+ self.loss_edge * self.lambda_edge
+        self.loss_style = self.criterionStyle(self.fake_B, self.real_B) * 10.0                  
+
+        self.loss_G = self.loss_G_GAN + loss_NCE_both + self.loss_style #+ self.loss_red #+ self.loss_edge * self.lambda_edge
         return self.loss_G
 
     def calculate_NCE_loss(self, src, tgt):
